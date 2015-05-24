0adebea20cebd5b992c4a6b459f34cee11a322f5
tattii <yuta.tatti@gmail.com>
2015-05-25 06:10:32 +0900

    add paper-fab(location, play)

--- map.js
+++ map.js
@@ -1,5 +1,9 @@
 
-var map = new L.Map("map", {center: new L.LatLng(37.892, 137.571), zoom: 6, zoomControl: false});
+var map = new L.Map("map", {
+	center: [37.892, 137.571], zoom: 6,
+	zoomControl: false,
+	attributionControl: false
+});
 
 var google = L.tileLayer(
 		'http://mt0.google.com/vt/lyrs=m@121&hl=ja&x={x}&y={y}&z={z}'
@@ -8,8 +12,8 @@ var google = L.tileLayer(
 var date = new Date(Date.now() - 5*60*1000); // 5 minutes before
 var refTime = date.getUTCFullYear() + pad(date.getUTCMonth()+1) + pad(date.getUTCDate()) + pad(date.getUTCHours()) + pad(Math.floor(date.getUTCMinutes()/5)*5);
 
-map.attributionControl.setPrefix(false);
-map.attributionControl.addAttribution("<a href='http://www.jma.go.jp/jp/highresorad/'>High-resolution Precipitation Nowcasts</a> at " + refTime + " UTC");
+//map.attributionControl.setPrefix(false);
+//map.attributionControl.addAttribution("<a href='http://www.jma.go.jp/jp/highresorad/'>High-resolution Precipitation Nowcasts</a> at " + refTime + " UTC");
 
 var nowcast = L.tileLayer.JMANowcast(
 		"http://www.jma.go.jp/jp/highresorad/highresorad_tile/HRKSNC/" + refTime + "/" + refTime + "/"
